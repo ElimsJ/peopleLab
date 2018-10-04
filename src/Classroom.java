f9053876935b1274e1cad3f4765b417e75edd731
Kent <kli8661@bths.edu>
2018-10-04 09:54:00 -0400

    Classroom1

--- src/Classroom.java
+++ src/Classroom.java
@@ -1,2 +1,27 @@
 public class Classroom {
+
+    Person[] students;
+    Person teacher;
+
+    public Classroom(Person[] students, Person teacher)
+    {
+        super("Classroom");
+        this.students = students;
+        this.teacher = teacher;
+    }
+
+    public String getSubject()
+    {
+
+    }
+
+    public double classAverage()
+    {
+
+    }
+
+    public String printClass()
+    {
+
+    }
 }
