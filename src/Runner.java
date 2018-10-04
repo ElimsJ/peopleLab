af562bf2cec062900c25738aa2be86cd23e3f567
BT_1N3_01 <sjiang9453@bths.edu>
2018-10-04 10:13:38 -0400

    Update

--- src/Runner.java
+++ src/Runner.java
@@ -1,2 +1,7 @@
 public class Runner {
+    public static void main(String [] args)
+    {
+        Person teacher = new Teacher("Science", "Mr","Shuyi","Jiang");
+    }
+
 }
--- src/Teacher.java
+++ src/Teacher.java
@@ -1,12 +1,20 @@
-public class Teacher extends {
+public class Teacher extends Person{
     String subject = "";
     String title = "";
-    public Teacher(String teacherName)
+    public Teacher(String subject,String title, String firstName, String familyName)
     {
-        super("Teacher");
-        this.pre
+        super(firstName,familyName);
+        this.subject = subject;
+        this.title = title;
     }
-
+    public  String getSubject(String subject)
+    {
+        return subject;
+    }
+    public  String getTitle(String titleName)
+    {
+        return titleName;
+    }
 
 }
 //RykoZin#0001
