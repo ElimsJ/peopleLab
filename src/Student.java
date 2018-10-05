25dca61e2ccd689b81fd372b97b5973942ad8b57
Kent <kli8661@bths.edu>
2018-10-05 09:07:41 -0400

    added student

--- src/Student.java
+++ src/Student.java
@@ -1,2 +1,20 @@
-public class Student {
+public class Student extends Person {
+    private double GPA;
+    private String familyName;
+    private String firstName;
+
+    public Student(String firstName, String familyName, double GPA) {
+        super(firstName, familyName);
+        this.GPA = GPA;
+        this.familyName = familyName;
+        this.firstName = firstName;
+    }
+
+    public double getGPA() {
+        return GPA;
+    }
+
+    public String toString() {
+        return familyName + " , " + firstName;
+    }
 }
