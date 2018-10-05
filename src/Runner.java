97d1955df18d5cd030f0570d2b418256bcd90543
BT_1N3_01 <sjiang9453@bths.edu>
2018-10-05 10:17:18 -0400

    d

--- src/Runner.java
+++ src/Runner.java
@@ -1,11 +1,21 @@
+import java.util.Random;
+
 public class Runner {
     static String[] firstName = {"Shuyi", "Kent", "Brian", "Wei", "Calvin", "Travis", "Vincent", "Jonathan", "Nathan", "Sheba", "Jack", "Dylan",};
     static String[] familyName = {"Gentleman", "Smith", "Li", "Lu", "Zheng", "Lin", "Rain", "Thunder", "Chair", "Table", "Matassa", "Leaf"};
+    public static Student randomStudent(Student Student[])
+    {
+        int rnd = new Random().nextInt(Student.length);
+        return Student[rnd];
+    }
     public static void main(String [] args)
     {
-        Teacher teacher = new Teacher("JAVA", "Mr.", "Sushi", "River");
-        Student student = new Student("Brian", "Lu");
-        System.out.println(teacher);
-        System.out.println(student);
+        Teacher teach = new Teacher("JAVA", "Mr ", "Sushi", "River");
+        Student[] student = new Student[12];
+        for(int i = 0; i < 12; i++)
+        {
+            student[i] = randomStudent();
+        }
+        Classroom theClass = new Classroom(student,teach);
     }
 }
